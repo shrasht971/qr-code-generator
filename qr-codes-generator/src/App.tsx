e24a3a823addbd5b9941430e7ed75a1a161f566e
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2025-04-30 00:20:54 +0530

    Some changes in routing

--- qr-codes-generator/src/App.tsx
+++ qr-codes-generator/src/App.tsx
@@ -1,11 +1,21 @@
 // src/App.tsx
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import {Route, Routes, Navigate } from "react-router-dom";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 
 const App: React.FC = () => {
   return (
     <div>
-      <Outlet />
+       <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
     </div>
   );
 };
--- qr-codes-generator/src/main.tsx
+++ qr-codes-generator/src/main.tsx
@@ -1,13 +1,15 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router.tsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 
