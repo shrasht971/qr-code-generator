72229e76e35af387b59286cb4dc00f9ac58dfdd8
shrasht971 <94127570+shrasht971@users.noreply.github.com>
2025-05-02 00:40:52 +0530

    Add const ts file

---
+++ src/const/Const.ts
@@ -0,0 +1,22 @@
+const LANGUAGES = {
+    en: {
+      welcome: "Welcome",
+      logout: "Logout",
+      profile: "Profile",
+      settings: "Settings",
+    },
+    hi: {
+      welcome: "स्वागत है",
+      logout: "लॉग आउट",
+      profile: "प्रोफ़ाइल",
+      settings: "सेटिंग्स",
+    },
+    fr: {
+      welcome: "Bienvenue",
+      logout: "Déconnexion",
+      profile: "Profil",
+      settings: "Paramètres",
+    },
+  };
+  
+  export default LANGUAGES;
